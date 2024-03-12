d67ae976694abef983cf4fa1a5bee2dde7a591f0
Sinth <sinthmite@gmail.com>
2024-03-12 19:09:06 -0400

    fifth commit

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const rockElement = document.getElementById('rock');
 const paperElement = document.getElementById('paper');
 const scissorsElement = document.getElementById('scissors');
 const pieces = document.getElementById('pieces');
+const piecesTwo = document.getElementById('piecesTwo');
 const userPick = document.getElementById('userPick');
 const cpuPick = document.getElementById('cpuPick');
+const number = document.getElementById('number');
 
 function cpu() {
     const random = Math.floor(Math.random() * 3);
@@ -13,8 +15,18 @@ function cpu() {
 
 function hidePieces() {
     pieces.style.display = 'none';
+    piecesTwo.style.display = 'none';
 }
 
+function showPiecesTwo() {
+    pieces.style.display = 'none';
+    piecesTwo.style.display = 'flex';
+}
+function showPiece(){
+    pieces.style.display = 'flex';
+    piecesTwo.style.display = 'none'; 
+}
+let score = 0
 function gamePlay(playerChoice) {
     const computerChoice = cpu(); // Call cpu() once and store the result
 
@@ -22,33 +34,43 @@ function gamePlay(playerChoice) {
         case 'rock':
             if (computerChoice === 'scissors') {
                 console.log('Player wins!');
-                setTimeout(()=>{
-                    cpuPick.src = "images/icon-scissors.svg"
-                }, 1000)
-                
+                score++;
+                number.innerHTML = score;
+                setTimeout(() => {
+                    cpuPick.src = "images/icon-scissors.svg";
+                    cpuPick.style.borderColor = 'hsl(230, 89%, 65%)';
+                    cpuPick.style.boxShadow = '1px 10px hsl(230, 89%, 40%), inset 0px 10px 1px 0px hsla(0, 0%, 0%, 0.1)';
+                }, 1000);
             } else if (computerChoice === 'paper') {
                 console.log('Computer wins!');
-                setTimeout(()=>{
-                    cpuPick.src = "images/icon-paper.svg"
-                }, 1000)
+                setTimeout(() => {
+                    cpuPick.src = "images/icon-paper.svg";
+                    cpuPick.style.borderColor = 'hsl(349, 70%, 56%)';
+                    cpuPick.style.boxShadow = '1px 10px hsl(349, 71%, 40%), inset 0px 10px 1px 0px hsla(0, 0%, 0%, 0.1)';
+                }, 1000);
             } else {
-                setTimeout(()=>{cpuPick.src=userPick.src;
-                }, 1000)
+                setTimeout(() => {
+                    cpuPick.src = userPick.src;
+                }, 1000);
                 console.log('It\'s a tie!');
-
             }
             break;
 
         case 'paper':
             if (computerChoice === 'rock') {
                 console.log('Player wins!');
+                score ++
+                number.innerHTML = score;
                 setTimeout(()=>{
                     cpuPick.src = "images/icon-rock.svg"
+                    
                 }, 1000)
             } else if (computerChoice === 'scissors') {
                 console.log('Computer wins!');
                 setTimeout(()=>{
                     cpuPick.src = "images/icon-scissors.svg"
+                    cpuPick.style.borderColor = ' hsl(40, 84%, 53%)'
+                    cpuPick.style.boxShadow = ' 1px 10px hsl(39, 89%, 40%), inset 0px 10px 1px 0px hsla(0, 0%, 0%, 0.1)'
                 }, 1000)
             } else {
                 setTimeout(()=>{cpuPick.src=userPick.src;
@@ -61,13 +83,20 @@ function gamePlay(playerChoice) {
         case 'scissors':
             if (computerChoice === 'paper') {
                 console.log('Player wins!');
+                score ++
+                number.innerHTML = score;
                 setTimeout(()=>{
                     cpuPick.src = "images/icon-paper.svg"
+                    cpuPick.style.borderColor = 'hsl(230, 89%, 65%)'
+                    cpuPick.style.boxShadow = '1px 10px  hsl(230, 89%, 40%), inset 0px 10px 1px 0px hsla(0, 0%, 0%, 0.1)'
+
                 }, 1000)
             } else if (computerChoice === 'rock') {
                 console.log('Computer wins!');
                 setTimeout(()=>{
                     cpuPick.src = "images/icon-rock.svg"
+                    cpuPick.style.borderColor = ' hsl(349, 70%, 56%)'
+                    cpuPick.style.boxShadow = '1px 10px  hsl(349, 71%, 40%), inset 0px 10px 1px 0px hsla(0, 0%, 0%, 0.1)'
                 }, 1000)
             } else {
                 setTimeout(()=>{cpuPick.src=userPick.src;
@@ -83,24 +112,40 @@ function gamePlay(playerChoice) {
     }
 
     hidePieces();
+    setTimeout(() => {
+        showPiecesTwo();
+        cpuPick.src = `images/icon-${computerChoice}.svg`;
+    }, 1000);
+    setTimeout(() => {
+        showPiece();
+    }, 4000);
 }
 
-
 rockElement.addEventListener('click', function() {
-    gamePlay('rock');
     userPick.src = "images/icon-rock.svg";
+    userPick.style.borderColor = 'hsl(230, 89%, 65%)';
+    userPick.style.boxShadow = '1px 10px hsl(230, 89%, 40%), inset 0px 10px 1px 0px hsla(0, 0%, 0%, 0.1)';
+    gamePlay('rock');
+    number.innerHTML = score;
 
 });
 
 paperElement.addEventListener('click', function() {
+    userPick.src = "images/icon-paper.svg";
+    userPick.style.borderColor = 'hsl(349, 70%, 56%)';
+    userPick.style.boxShadow = '1px 10px hsl(349, 71%, 40%), inset 0px 10px 1px 0px hsla(0, 0%, 0%, 0.1)';
     gamePlay('paper');
-    userPick.src = "images/icon-rock.svg"
-
+    number.innerHTML = score;
 
 });
 
 scissorsElement.addEventListener('click', function() {
+    userPick.src = "images/icon-scissors.svg";
+    userPick.style.borderColor = ' hsl(40, 84%, 53%)'
+    userPick.style.boxShadow = ' 1px 10px hsl(39, 89%, 40%), inset 0px 10px 1px 0px hsla(0, 0%, 0%, 0.1)'
     gamePlay('scissors');
-    userPick.src = "images/icon-rock.svg"
+    number.innerHTML = score;
 
 });
+
+console.log(score)
