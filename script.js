d2160127e7f77afe241e6651e9364a5cfe3e823f
Sinth <sinthmite@gmail.com>
2024-03-12 02:21:14 -0400

    third commit

---
+++ script.js
@@ -0,0 +1,11 @@
+rock = document.getElementById('rock');
+paper = document.getElementById('paper');
+scissor = document.getElementById('scissors');
+
+function cpu(){
+    Math.floor(Math.random()*3)
+}
+
+function gamePlay(){
+
+}
